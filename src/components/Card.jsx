b10588205b2c4dba09941bf2f6492fdc661653f1
ssmahim01 <mahimzayn666@gmail.com>
2024-11-04 19:48:31 +0600

    Updated images link in JSON file of products and Edited Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({ product }) => {
   return (
     <div className="bg-white p-5 rounded-2xl space-y-3">
       <img
-        className="w-full h-44 rounded-xl"
+        className="w-full h-44 bg-slate-200 rounded-xl"
         src={product_image}
         alt={product_title}
       />
@@ -15,7 +15,7 @@ const Card = ({ product }) => {
 
       <p className="text-gray-500 font-medium">Price: {price}k</p>
 
-      <button className="py-2 px-4 hover:btn-ghost text-textColor font-semibold border-2 border-btnBorder rounded-full">
+      <button className="py-2 px-4 hover:btn-ghost text-sm text-textColor font-semibold border-2 border-btnBorder rounded-full">
         View Details
       </button>
       <Link to={`product/${product_id}`}></Link>
